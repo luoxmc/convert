6a03724aa3c803c41cf4ae0ebd1e5fec6b151e84
luoxmc <luoxmc@vip.qq.com>
2018-03-23 21:00:50 +0800

    修改百度统计代码

--- src/main/webapp/convert/plugins/baidu.js
+++ src/main/webapp/convert/plugins/baidu.js
@@ -1,7 +1,8 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2c4d2ec6b86f95afdebd3fc79ad9b1b4";
+  hm.src = "https://hm.baidu.com/hm.js?070dbebe2dd14ff8fe83d5fba749e3a4";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
